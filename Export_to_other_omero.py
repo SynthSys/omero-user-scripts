e86a36a62d6b08ea17e3b5ab23bed461acf323ed
eilidh-t <e.troup@epcc.ed.ac.uk>
2017-11-23 16:26:58 +0000

    Transfers file from hard-coded path. Next to get file selected by user.

--- Export_to_other_omero.py
+++ Export_to_other_omero.py
@@ -10,12 +10,15 @@
 # the scripting service.
 import omero
 from omero.rtypes import rlong, rstring, unwrap
-from omero.gateway import BlitzGateway
-import omero.scripts as scripts
-import omero.fs
 from path import path
-import sys
-import subprocess
+import os
+import omero.cli
+import omero.scripts as scripts
+from omero.gateway import BlitzGateway
+
+
+REMOTE_HOST = 'demo.openmicroscopy.org'
+REMOTE_PORT = 4064
 
 # Script definition
 
@@ -56,73 +59,38 @@ for f in image.getImportedImageFiles():
    print f.path,f.name
 
 # Connect to remote omero
-remote_host = 'demo.openmicroscopy.org'
-remote_port = 4064
-
-c = omero.client(host=remote_host, port=remote_port, args=["--Ice.Config=/dev/null"])
+c = omero.client(host=REMOTE_HOST, port=REMOTE_PORT, args=["--Ice.Config=/dev/null"])
 c.createSession(username, password)
 try:
+    # Just to test connection, print the projects.
     remote_conn = BlitzGateway(client_obj=c)
     for p in remote_conn.getObjects("Project"):
 	    print p.id, p.name
 
-
-    # Transfer image over
-    key = c.getSessionId()
-
-    args = [sys.executable]
-    args.append(str(path(".") / "bin" / "omero"))
-    args.extend(
-        ["-s", remote_host, "-k", key, "-p", str(remote_port), "import"])
+    print "Connected to ", REMOTE_HOST, ", now to transfer image"
+    cli = omero.cli.CLI()
+    cli.loadplugins()
+    cli.set_client(c)
+    del os.environ["ICE_CONFIG"]
 
     # TODO Find location of ManagedRepository
     #TODO get selected file from user choice
-
     file_loc = "/Users/eilidhtroup/omero/ManagedRepository/root_0/2017-10/06/" \
                "16-21-46.945/antibiotic_plate.jpg"
-    # file_loc = "root_0/2017-10/06/" \
-    #            "16-21-46.945/antibiotic_plate.jpg"
-    args.append(file_loc)
+    cli.invoke(["import", file_loc])
 
-    print "args are"
-    print args
-
-    # TODO could get omero_dist as done in def omerodistdir(cls):
-    # from https://github.com/openmicroscopy/openmicroscopy/blob/develop/components/tools/OmeroPy/src/omero/testlib/__init__.py#L277
-    popen = subprocess.Popen(args,  # cwd=str(self.omero_dist),
-                             cwd="/Users/eilidhtroup/Documents/SynthSysDataManagement/omero/omeroServer/OMERO.server",
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-    out, err = popen.communicate()
-    rc = popen.wait()
-    if rc != 0:
-        raise Exception("import failed: [%r] %s\n%s" % (args, rc, err))
-    pix_ids = []
-    print "Output"
-    for x in out.split("\n"):
-        print x
-        if x and x.find("Created") < 0 and x.find("#") < 0:
-            try:  # if the line has an image ID...
-                image_id = str(long(x.strip()))
-                # Occasionally during tests an id is duplicated on stdout
-                if image_id not in pix_ids:
-                    pix_ids.append(image_id)
-            except:
-                pass
-
-    print "pix_ids"
-    print pix_ids
     # End of transferring image
 finally:
+    remote_conn.close()
     c.closeSession()
-   # remote_conn.close()
+
 # Return some value(s).
 
 # Here, we return anything useful the script has produced.
 # NB: The Insight and web clients will display the "Message" output.
 
-msg = "Script ran with Image ID: %s, Name: %s and \nUsername: %s"\
-      % (image_id, image.getName(), username)
-client.setOutput("Message", rstring(msg))
+# msg = "Script ran with Image ID: %s, Name: %s and \nUsername: %s"\
+#       % (image_id, image.getName(), username)
+# client.setOutput("Message", rstring(msg))
 
 client.closeSession()
